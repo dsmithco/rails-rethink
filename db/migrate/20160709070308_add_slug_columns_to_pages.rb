f740f7f2cc2b9decddbd734dd718d3e3d0ae5507
dsmithco <dsmithco@gmail.com>
2016-07-09 13:53:22 -0700

    updating for deploy

--- db/migrate/20160709070308_add_slug_columns_to_pages.rb
+++ db/migrate/20160709070308_add_slug_columns_to_pages.rb
@@ -3,6 +3,6 @@ class AddSlugColumnsToPages < ActiveRecord::Migration[5.0]
     # add_column   :friendly_id_slugs, :scope, :string
     # remove_index :friendly_id_slugs, [:slug, :sluggable_type]
     # add_index    :friendly_id_slugs, [:slug, :sluggable_type]
-    add_index    :friendly_id_slugs, [:slug, :sluggable_type, :scope], unique: true
+    # add_index    :friendly_id_slugs, [:slug, :sluggable_type, :scope], unique: true
   end
 end
