89604439967c11ca9f73c90ac1a746857a6157a2
dsmithco <dsmithco@gmail.com>
2016-07-09 19:01:16 -0700

    setting default theme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_website
+
+  def set_website
+    @website = Website.where("domain_url like ?", "%#{request.host}%").first
+    ApplicationController.layout "themes/#{@website.theme}"
+  end
 end
