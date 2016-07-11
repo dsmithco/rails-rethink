48bef7b5ffffa221fd50686f6996110eb0619492
dsmithco <dsmithco@gmail.com>
2016-07-10 18:02:11 -0700

    compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.precompile += %w( .svg .eot .woff .ttf )
+  config.assets.precompile += %w( .svg .eot .woff .ttf .woff2)
 
   config.assets.compile = true
 
