6aada6dab03b8ebd09971930352f4596d972146d
dsmithco <dsmithco@gmail.com>
2016-07-10 18:10:14 -0700

    compile

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,11 @@ Bundler.require(*Rails.groups)
 
 module RailsRethink
   class Application < Rails::Application
-
+    config.assets.precompile << Proc.new { |path|
+      if path =~ /\.(eot|svg|ttf|woff)\z/
+        true
+      end
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
