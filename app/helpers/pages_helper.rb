68bcbce94125f8c3b7d661f1bb9c44538e36a4bf
dsmithco <dsmithco@gmail.com>
2016-10-01 13:23:00 -0700

    Updating redirect url handling for redirectable pages

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,10 +1,10 @@
 module PagesHelper
 
-  def page_path(page)
+  def page_path(page, redirect=nil)
 
     url = "/pages/#{page.slug}"
 
-    if page.redirectable_url.present? && request.method == 'get'
+    if page.redirectable_url.present? && redirect.present?
       url = page.redirectable_url
     elsif page.page_id.present?
       url = "/pages/#{page.page.slug}/#{page.slug}"
