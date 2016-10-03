a470ca6c2f6d93759fa363d8c53359f5fdb62622
dsmithco <dsmithco@gmail.com>
2016-10-03 12:59:52 -0700

    Updating block

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy, :search]
-  load_and_authorize_resource
+  load_and_authorize_resource except: [:search]
 
   # GET /categories
   # GET /categories.json
