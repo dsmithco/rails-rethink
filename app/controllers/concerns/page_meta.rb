780927bd868d6702215b48230f248ba7f841d1b1
dsmithco <dsmithco@gmail.com>
2016-10-08 18:54:39 -0700

    Adding better search engine meta data  capabilities

--- app/controllers/concerns/page_meta.rb
+++ app/controllers/concerns/page_meta.rb
@@ -7,7 +7,7 @@ module PageMeta
 
   def set_site_meta_data
     @resource = instance_variable_get("@#{self.controller_name.singularize}")
-    @site_title = "#{@resource.name} - #{@site_title}" if @resource.name.present?
+    @site_title = "#{@resource.name} - #{@current_website.name}" if @resource.name.present?
     @site_title = "#{@site_title} - #{@resource.subtitle}" if @resource.subtitle.present?
     @site_description = @resource.description.present? ? "#{@resource.description}" : "#{ActionController::Base.helpers.truncate(ActionController::Base.helpers.strip_tags(@resource.about), length: 250)}"
   end
